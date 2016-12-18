996114573d39669d069c53880ad3070c9e1fe7f8
temoteam <lohness@lohness.com>
2016-12-18 03:21:47 +0300

    this is the first commit of this app

---
+++ Холодильник/RcpPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using AppStudio.DataProviders;
+using AppStudio.DataProviders.Core;
+using AppStudio.DataProviders.RestApi;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Холодильник
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RcpPage : Section<Schema1Schema>
+
+    {
+        String enabled;
+        public RcpPage()
+        {
+            this.InitializeComponent();
+            _dataProvider = new RestApiDataProvider();
+
+
+        }
+
+        private RestApiDataProvider _dataProvider;
+
+        
+
+        public override async Task<IEnumerable<Schema1Schema>> GetDataAsync(SchemaBase connectedItem = null)
+        {
+            var config = new RestApiDataConfig
+            {
+                Url = new Uri("https://api.vk.com/method/wall.get?owner_id=-44235988&count=1"),
+                PaginationConfig = new PageNumberPagination("offset", false, "")
+            };
+
+            return await _dataProvider.LoadDataAsync(config, MaxRecords, new Schema1SchemaParser());
+        }
+
+        public override async Task<IEnumerable<Schema1Schema>> GetNextPageAsync()
+        {
+            return await _dataProvider.LoadMoreDataAsync<Schema1Schema>();
+        }
+
+        public override bool HasMorePages
+        {
+            get
+            {
+                return _dataProvider.HasMoreItems;
+            }
+        }
+
+        public override ListPageConfig<Schema1Schema> ListPage
+        {
+            get
+            {
+                return new ListPageConfig<Schema1Schema>
+                {
+                    Title = "Новости",
+
+                    Page = typeof(Pages.Section1ListPage),
+
+                    LayoutBindings = (viewModel, item) =>
+                    {
+                        viewModel.SubTitle = item.text.ToSafeString();
+                        viewModel.ImageUrl = ItemViewModel.LoadSafeUrl(item.src.ToSafeString());
+                    },
+                    DetailNavigation = (item) =>
+                    {
+                        return NavInfo.FromPage<Pages.Section1DetailPage>(true);
+                    }
+                };
+            }
+        }
+
+        public override DetailPageConfig<Schema1Schema> DetailPage
+        {
+            get
+            {
+                var bindings = new List<Action<ItemViewModel, Schema1Schema>>();
+                bindings.Add((viewModel, item) =>
+                {
+                    viewModel.PageTitle = "Post";
+                    viewModel.Title = "";
+                    viewModel.Description = item.text.ToSafeString();
+                    viewModel.ImageUrl = ItemViewModel.LoadSafeUrl(item.src.ToSafeString());
+                    viewModel.Content = null;
+                });
+
+                var actions = new List<ActionConfig<Schema1Schema>>
+                {
+                };
+
+                return new DetailPageConfig<Schema1Schema>
+                {
+                    Title = "Новости",
+                    LayoutBindings = bindings,
+                    Actions = actions
+                };
+            }
+        }
+
+        
+    }
+    public class Schema1Schema : SchemaBase
+    {
+
+        public string text { get; set; }
+
+        public string src { get; set; }
+    }
+
+}
---
+++ Холодильник/SelectPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using AppStudio;
+using AppStudio.DataProviders.RestApi;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Холодильник
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SelectPage : Page
+    {
+        private RestApiDataProvider _dataProvider;
+
+        Dictionary<String, int> a;
+        String enabled = "";
+        public SelectPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            a  = new Dictionary<string, int>();
+            a.Add("vjkjrj", 32);
+
+        }
+
+        public void getFood()
+        {
+            this.Frame.Navigate(typeof(RcpPage), enabled);
+            _dataProvider = new RestApiDataProvider();
+
+        }
+
+        private void checkBox_Click(object sender, System.EventArgs e)
+        {
+            CheckBox checkBox = new CheckBox();
+            checkBox = sender as CheckBox;
+            if (checkBox.IsChecked.Value)
+            {
+                enabled = enabled + a[checkBox.Content.ToString()] + ",";
+                
+            }
+            else
+            {
+                enabled.Replace(a[checkBox.Content.ToString()]+",", "");
+            }
+        }
+
+
+
+
+    }
+}
